98e74f80b174d74bc773efbc9bf8b077bab06e23
yejianlin <1939799477@qq.com>
2016-01-15 17:25:00 +0800

    修改语音

--- IflytekVoice/src/main/java/com/alinge/software/iflytekvoice/VoiceHelperActivity.java
+++ IflytekVoice/src/main/java/com/alinge/software/iflytekvoice/VoiceHelperActivity.java
@@ -1,7 +1,21 @@
 package com.alinge.software.iflytekvoice;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.alinge.software.iflytekvoice.recognizer.IflytekSynthesizer;
+import com.alinge.software.iflytekvoice.recognizer.code.Code;
+import com.alinge.software.iflytekvoice.utils.LogUtils;
 
 /**
  * 作者： yejianlin
@@ -9,9 +23,63 @@ import android.os.Bundle;
  * 作用：
  */
 public class VoiceHelperActivity extends Activity {
+    private ImageView imageView;
+    private IflytekSynthesizer synthesizer;
+    private RecognizerReceiver mRecognizerReceiver;
+    private  SynthesizerReceiver mSynthesizerReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helper);
+
+        imageView= (ImageView) findViewById(R.id.helper);
+        synthesizer = new IflytekSynthesizer(this);
+        initReceiver();
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               v.setBackgroundResource(R.drawable.voice_animation);
+                AnimationDrawable drawable=(AnimationDrawable)v.getBackground();
+                synthesizer.synthesizer("早上好！小朋友");
+                drawable.start();
+
+            }
+        });
+
+
     }
+    private void initReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Code.RECOGNIZER_ACTION);
+        filter.addAction(Code.UNDERSTANDER_ACTION);
+        mRecognizerReceiver = new RecognizerReceiver();
+        registerReceiver(mRecognizerReceiver, filter);
+
+        IntentFilter synthesizerFilter=new IntentFilter();
+        synthesizerFilter.addAction(Code.SYNTHESIZER_ACTION);
+        SynthesizerReceiver msynSynthesizerReceiver=new SynthesizerReceiver();
+        registerReceiver(msynSynthesizerReceiver,synthesizerFilter);
+
+    }
+    private class RecognizerReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int code = intent.getIntExtra(Code.STATUS_CODE, 0);
+            String result = intent.getStringExtra(Code.RECOGNIZER_RESULT);
+        }
+    }
+    private class SynthesizerReceiver extends  BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int code=intent.getIntExtra(Code.STATUS_CODE, 0);
+            LogUtils.info("1/","systhesizer code:"+code);
+            if(code==Code.BEGIN_SPEEK){
+                //开始播放
+            }/*else if(){
+
+            }*/
+        }
+    }
+
 }
--- IflytekVoice/src/main/java/com/alinge/software/iflytekvoice/recognizer/IflytekSynthesizer.java
+++ IflytekVoice/src/main/java/com/alinge/software/iflytekvoice/recognizer/IflytekSynthesizer.java
@@ -115,14 +115,14 @@ public class IflytekSynthesizer {
         mSynthesizer.setParameter(SpeechConstant.PARAMS, null);
         mSynthesizer.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
         // 设置在线合成发音人
-        mSynthesizer.setParameter(SpeechConstant.VOICE_NAME, "nannan");
+        mSynthesizer.setParameter(SpeechConstant.VOICE_NAME, "aisxa");
         //设置合成语速
-        mSynthesizer.setParameter(SpeechConstant.SPEED, "50");
-        mSynthesizer.setParameter(SpeechConstant.BACKGROUND_SOUND,"1");
+        mSynthesizer.setParameter(SpeechConstant.SPEED, "90");
+      //  mSynthesizer.setParameter(SpeechConstant.BACKGROUND_SOUND,"1");
         //设置合成音调
-        mSynthesizer.setParameter(SpeechConstant.PITCH, "50");
+        mSynthesizer.setParameter(SpeechConstant.PITCH, "70");
         //设置合成音量
-        mSynthesizer.setParameter(SpeechConstant.VOLUME, "50");
+        mSynthesizer.setParameter(SpeechConstant.VOLUME, "100");
         //设置播放器音频流类型
         mSynthesizer.setParameter(SpeechConstant.STREAM_TYPE, "3");
         // 设置播放合成音频打断音乐播放，默认为true
