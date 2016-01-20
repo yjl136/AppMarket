bc87f2025b99b392812249ed3a2fe204333bcdec
yejianlin <1939799477@qq.com>
2016-01-20 17:33:05 +0800

    添加homeFragment_backup

--- app/src/main/java/com/alinge/software/market/fragment/HomeFragment_backup.java
+++ app/src/main/java/com/alinge/software/market/fragment/HomeFragment_backup.java
@@ -46,7 +46,7 @@ public class HomeFragment_backup extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View content = inflater.inflate(R.layout.fragment_home, container, false);
+        View content = inflater.inflate(R.layout.fragment_home_backup, container, false);
         initView(content);
         initDatas();
         initEvents();
